6d6d5581a63a560047b31c682c8229fa0c357c3d
alanrslima <alanronison.lima@gmail.com>
2020-10-23 09:30:28 -0300

    Included Procfile

--- src/server.ts
+++ src/server.ts
@@ -13,4 +13,6 @@ app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server started on port ${process.env.PORT || 3333}`);
+});
